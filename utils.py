23d37c3bb927199376b58da926b1cc159b21dd1d
liuyoude <2859648675@qq.com>
2022-05-30 20:40:03 +0800

    update function of get_filename_list

--- utils.py
+++ utils.py
@@ -42,17 +42,18 @@ def get_logger(filename):
     logger.addHandler(logging.StreamHandler())
     return logger
 
-def get_filename_list(dir_path, ext=None):
+
+def get_filename_list(dir_path, pattern='*', ext='*'):
     """
     find all extention files under directory
     :param dir_path: directory path
     :param ext: extention name, like wav, png...
+    :param pattern: filename pattern for searching
     :return: files path list
     """
     filename_list = []
-    ext = ext if ext else '*'
     for root, dirs, files in os.walk(dir_path):
-        file_path_pattern = os.path.join(root, f'*.{ext}')
+        file_path_pattern = os.path.join(root, f'{pattern}.{ext}')
         files = sorted(glob.glob(file_path_pattern))
         filename_list += files
     return filename_list
