8f5fe6b6a5a32f7dd8b85085c48b0d6c41a1f485
liuyoude <2859648675@qq.com>
2022-08-30 09:23:17 +0800

    fix bug

--- run.py
+++ run.py
@@ -76,7 +76,7 @@ def run():
     writer = SummaryWriter(log_dir=log_dir)
     logger = utils.get_logger(filename=os.path.join(log_dir, 'running.log'))
     # save / load version files
-    if args.save_version_files: utils.save_load_version_files(log_dir, args.load_epoch, args.save_version_file_patterns)
+    if args.save_version_files: utils.save_load_version_files(log_dir, args.load_epoch, args.save_version_file_patterns, args.pass_dirs)
     # save config file
     utils.save_yaml_file(file_path=os.path.join(log_dir, 'config.yaml'), data=vars(args))
     # run
@@ -85,7 +85,7 @@ def run():
     main(args)
     # restore latest version files
     if args.save_version_files and args.load_epoch:
-        utils.copy_files(f'.{sep}runs{sep}latest_project', f'.{sep}', args.save_version_file_patterns, pass_dirs)
+        utils.copy_files(f'.{sep}runs{sep}latest_project', f'.{sep}', args.save_version_file_patterns, args.pass_dirs)
 
 
 if __name__ == '__main__':
